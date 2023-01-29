c73023922231601187d7286cd1bac87b26b82dd9
Susmito Bhattacharyya <susmito96@hotmail.com>
2023-01-29 23:18:02 +0530

    Day01-01292023-FirstCommit

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+ * GLOBALS - NO WINDOW, as we are not in the browser here,
+ * __dirname  - path to current directory
+ * __filename - file name
+ * require    - function to use modules (CommonJS)
+ * module     - info about current module (file)
+ * process    - info about the current environment where the program is being executed
+ */
+
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
+console.log(process);
