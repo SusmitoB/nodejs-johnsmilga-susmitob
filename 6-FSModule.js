a142c39d5026950e5129c79cf02604f26e97e0c3
Susmito Bhattacharyya <susmito96@hotmail.com>
2023-01-30 13:13:04 +0530

    Day02-01302023-TillfsModules

---
+++ 6-FSModule.js
@@ -0,0 +1,32 @@
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+const os = require('os');
+
+// *if you don't give the 2nd arg, the log will show buffered value and not the expected string.
+const first = readFileSync('contents/first.txt', 'utf8');
+const second = readFileSync('contents/second.txt', 'utf-8');
+
+console.log({
+  first,
+  second,
+});
+
+writeFileSync('contents/subFolder/writtenFile.txt', `I am writing this from the ${path.basename(__filename)}!`);
+
+// * this approach will overwrite the content
+// setInterval(() => {
+//   const freeMemory = os.freemem();
+//   const currentTime = new Date().toLocaleString();
+//   writeFileSync('contents/subFolder/freeMemoryRecord.txt', `Free memory at ${currentTime} is: ${freeMemory}\n`);
+// }, 2000);
+
+// * but here we want to keep the record so to do it we will be passing {flag: 'a'} in the arguments of writeFileSync
+setInterval(() => {
+  const freeMemory = os.freemem();
+  const totalMemory = os.totalmem();
+  const freePercentage = (freeMemory / totalMemory) * 100;
+  const currentTime = new Date().toLocaleString();
+  writeFileSync('contents/subFolder/freeMemoryRecord.txt', `Free memory at ${currentTime} is: ${freePercentage.toFixed(2)}%\n`, {
+    flag: 'a',
+  });
+}, 2000);
