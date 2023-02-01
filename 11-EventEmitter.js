a3e9816c4beb1e241332e8f5a6cba95d6594a6d6
Susmito Bhattacharyya <susmito96@hotmail.com>
2023-02-01 12:19:24 +0530

    Day04-02012023-TillEventEmitter

--- 11-EventEmitter.js
+++ 11-EventEmitter.js
@@ -2,6 +2,12 @@ const EventEmitter = require('events');
 
 const customEmitter = new EventEmitter();
 
+/*
+ * first we write the 'on' then the 'emit'
+ * we can pass the arguments from emit
+ * some modules in node use the EvenEmitter under the hood, eg.- http module.
+ */
+
 customEmitter.on('response', () => {
   console.log('data recieved');
 });
@@ -15,4 +21,5 @@ customEmitter.on('userData', ({ userName = '', age = 0, gender = '' }) => {
   console.log(`${userName} is a ${gender} and ${age} years old.`);
 });
 
+// Passing the argument from emitter
 customEmitter.emit('userData', { userName: 'Susmito', age: '27', gender: 'male' });
